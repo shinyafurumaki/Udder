8812fe70aae4f2835f450bfb53acf35024cf2695
shinya furumaki <furumakishinya@mac.com>
2019-01-27 17:37:00 +1300

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by 古牧晋弥 on 27/01/19.
+//  Copyright © 2019 Shinya Furumaki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
